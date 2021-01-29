8b0f4afdf32b5a6082d4ba76e9ea62a7b6cce0c2
mps1209 <zyj19961209@gmail.com>
2021-01-29 13:10:55 +0800

    Add files via upload

---
+++ miaona-soga.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+    clear
+    echo "错误：本脚本需要 root 权限执行。" 1>&2
+    exit 1
+fi
+
+download_miaona(){
+	echo "正在安装soga . . ."
+	bash <(curl -Ls https://blog.sprov.xyz/soga.sh)
+	echo "正在同步时间 . . ."
+	yum install -y ntp
+	systemctl enable ntpd
+	ntpdate -q 0.rhel.pool.ntp.org
+	systemctl restart ntpd
+	echo "正在更新配置文件 . . ."
+	rm -f /etc/soga/soga.conf
+	wget -P /etc/v2ray https://raw.githubusercontent.com/mps1209/miaoconfig/master/soga.conf
+	cd /etc/soga
+	printf "请输入节点ID："
+	read -r nodeId <&1
+	sed -i "s/ID_HERE/$nodeId/" soga.conf
+	service v2ray restart
+	shon_online
+}
+
+start_miaona(){
+	echo "正在启动soga . . ."
+	soga start
+	shon_online
+}
+
+shon_online(){
+echo "请选择您需要进行的操作:"
+echo "1) 安装 soga"
+echo "2) 启动 soga"
+echo "3) 查看 soga状态"
+echo "4) 退出脚本"
+echo "   Version：1.0.0"
+echo ""
+echo -n "   请输入编号: "
+read N
+case $N in
+  1) download_miaona ;;
+  2) start_miaona ;;
+  3) soga status ;;
+  4) exit ;;
+  *) echo "Wrong input!" ;;
+esac 
+}
+
+shon_online
